bc8f92ee594c7eb07f7c9a51e76750ee9066ad88
rsumii <ryobasketball0408@gmail.com>
2022-07-12 18:03:11 +0900

    fixed bug

--- migrations/20220705085442_add_user_to_user_table.postgres.up.sql
+++ migrations/20220705085442_add_user_to_user_table.postgres.up.sql
@@ -1,4 +1,4 @@
 INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at)
 VALUES
-('Sumii','Ryo','admi@admin.com','$2a$12$93Qx42Wt6o8oJk547bomUOrY9oRuCIW5UG5xF7Ff4oTmErGDfP05m',3,'2022-06-29 00:00:00','2022-06-29 00:00:00');
+('Sumii','Ryo','admin@admin.com','$2a$12$93Qx42Wt6o8oJk547bomUOrY9oRuCIW5UG5xF7Ff4oTmErGDfP05m',3,'2022-06-29 00:00:00','2022-06-29 00:00:00');
 
